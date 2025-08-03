65e53ef6b23d1ada3956bd197ded2756beefb89c
Serenity <34622204+oSerenity@users.noreply.github.com>
2025-08-03 00:56:07 -0400

    Updated XNotify

--- JRPC_Client/JRPC.cs
+++ JRPC_Client/JRPC.cs
@@ -10,6 +10,105 @@ namespace JRPC_Client
 {
     public static class JRPC
     {
+        #region Enums
+        public enum TemperatureType
+        {
+            CPU,
+            GPU,
+            EDRAM,
+            MotherBoard
+        };
+        public enum ThreadType
+        {
+            System,
+            Title
+        }
+        public enum XNotiyLogo : uint
+        {
+            XBOX_LOGO = 0,
+            NEW_MESSAGE_LOGO = 1,
+            FRIEND_REQUEST_LOGO = 2,
+            NEW_MESSAGE = 3,
+            FLASHING_XBOX_LOGO = 4,
+            GAMERTAG_SENT_YOU_A_MESSAGE = 5,
+            GAMERTAG_SINGED_OUT = 6,
+            GAMERTAG_SIGNEDIN = 7,
+            GAMERTAG_SIGNED_INTO_XBOX_LIVE = 8,
+            GAMERTAG_SIGNED_IN_OFFLINE = 9,
+            GAMERTAG_WANTS_TO_CHAT = 10,
+            DISCONNECTED_FROM_XBOX_LIVE = 11,
+            DOWNLOAD = 12,
+            FLASHING_MUSIC_SYMBOL = 13,
+            FLASHING_HAPPY_FACE = 14,
+            FLASHING_FROWNING_FACE = 15,
+            FLASHING_DOUBLE_SIDED_HAMMER = 16,
+            GAMERTAG_WANTS_TO_CHAT_2 = 17,
+            PLEASE_REINSERT_MEMORY_UNIT = 18,
+            PLEASE_RECONNECT_CONTROLLERM = 19,
+            GAMERTAG_HAS_JOINED_CHAT = 20,
+            GAMERTAG_HAS_LEFT_CHAT = 21,
+            GAME_INVITE_SENT = 22,
+            FLASH_LOGO = 23,
+            PAGE_SENT_TO = 24,
+            FOUR_2 = 25,
+            FOUR_3 = 26,
+            ACHIEVEMENT_UNLOCKED = 27,
+            FOUR_9 = 28,
+            GAMERTAG_WANTS_TO_TALK_IN_VIDEO_KINECT = 29,
+            VIDEO_CHAT_INVITE_SENT = 30,
+            READY_TO_PLAY = 31,
+            CANT_DOWNLOAD_X = 32,
+            DOWNLOAD_STOPPED_FOR_X = 33,
+            FLASHING_XBOX_CONSOLE = 34,
+            X_SENT_YOU_A_GAME_MESSAGE = 35,
+            DEVICE_FULL = 36,
+            FOUR_7 = 37,
+            FLASHING_CHAT_ICON = 38,
+            ACHIEVEMENTS_UNLOCKED = 39,
+            X_HAS_SENT_YOU_A_NUDGE = 40,
+            MESSENGER_DISCONNECTED = 41,
+            BLANK = 42,
+            CANT_SIGN_IN_MESSENGER = 43,
+            MISSED_MESSENGER_CONVERSATION = 44,
+            FAMILY_TIMER_X_TIME_REMAINING = 45,
+            DISCONNECTED_XBOX_LIVE_11_MINUTES_REMAINING = 46,
+            KINECT_HEALTH_EFFECTS = 47,
+            FOUR_5 = 48,
+            GAMERTAG_WANTS_YOU_TO_JOIN_AN_XBOX_LIVE_PARTY = 49,
+            PARTY_INVITE_SENT = 50,
+            GAME_INVITE_SENT_TO_XBOX_LIVE_PARTY = 51,
+            KICKED_FROM_XBOX_LIVE_PARTY = 52,
+            NULLED = 53,
+            DISCONNECTED_XBOX_LIVE_PARTY = 54,
+            DOWNLOADED = 55,
+            CANT_CONNECT_XBL_PARTY = 56,
+            GAMERTAG_HAS_JOINED_XBL_PARTY = 57,
+            GAMERTAG_HAS_LEFT_XBL_PARTY = 58,
+            GAMER_PICTURE_UNLOCKED = 59,
+            AVATAR_AWARD_UNLOCKED = 60,
+            JOINED_XBL_PARTY = 61,
+            PLEASE_REINSERT_USB_STORAGE_DEVICE = 62,
+            PLAYER_MUTED = 63,
+            PLAYER_UNMUTED = 64,
+            FLASHING_CHAT_SYMBOL = 65,
+            UPDATING = 76,
+        }
+        public enum XboxRebootFlags
+        {
+            Title = 0,
+            Wait = 1,
+            Cold = 2,
+            Warm = 4,
+            Stop = 8
+        }
+        public enum LEDState
+        {
+            OFF = 0x00,
+            RED = 0x08,
+            GREEN = 0x80,
+            ORANGE = 0x88,
+        };
+        #endregion
         private static readonly uint Void = 0,
             Int = 1,
             String = 2,
@@ -569,14 +668,6 @@ namespace JRPC_Client
             return uint.Parse(Responce.Substring(Responce.find(" ") + 1));
         }
 
-        public enum LEDState
-        {
-            OFF = 0x00,
-            RED = 0x08,
-            GREEN = 0x80,
-            ORANGE = 0x88,
-        };
-
         public static void SetLeds(this IXboxConsole console, LEDState Top_Left, LEDState Top_Right, LEDState Bottom_Left, LEDState Bottom_Right)
         {
             string cmd = "consolefeatures ver=" + JRPCVersion + " type=14 params=\"A\\0\\A\\4\\" + 
@@ -586,14 +677,6 @@ namespace JRPC_Client
                 JRPC.Int + "\\" + (uint)Bottom_Right + "\\\"",
             Responce = SendCommand(console, cmd);
         }
-
-        public enum TemperatureType
-        {
-            CPU,
-            GPU,
-            EDRAM,
-            MotherBoard
-        };
 
         public static uint GetTemperature(this IXboxConsole console, TemperatureType TemperatureType)
         {
@@ -605,14 +688,13 @@ namespace JRPC_Client
 
         public static void XNotify(this IXboxConsole console, string Text)
         {
-            XNotify(console, Text, 34);
+            XNotify(console, XNotiyLogo.FLASHING_XBOX_CONSOLE, Text);
         }
 
-        public static void XNotify(this IXboxConsole console, string Text, uint Type)
+        public static void XNotify(this IXboxConsole console, XNotiyLogo Type, string Text)
         {
             string cmd = "consolefeatures ver=" + JRPCVersion + " type=12 params=\"A\\0\\A\\2\\" + 
-                JRPC.String + "/" + Text.Length + "\\" + Text.ToHexString() + "\\" + 
-                JRPC.Int + "\\" + Type + "\\\"";
+               String + "/" + Text.Length + "\\" + Text.ToHexString() + "\\" + Int + "\\" + Type + "\\\"";
             SendCommand(console, cmd);
         }
 
@@ -682,12 +764,6 @@ namespace JRPC_Client
                 return JRPC.Uint64;
             }
             return JRPC.Uint64;
-        }
-
-        public enum ThreadType
-        {
-            System,
-            Title
         }
 
         public static T Call<T>(this IXboxConsole console, uint Address, params object[] Arguments) where T : struct
@@ -1224,7 +1300,7 @@ namespace JRPC_Client
                     }
                 }
 
-                if (Type == JRPC.Void)
+                if (Type == Void)
                     return 0;
                 return ulong.Parse(Responce.Substring(Responce.find(" ") + 1), System.Globalization.NumberStyles.HexNumber);
             }
